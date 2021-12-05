4a575193fcb9b1b01a15d8fb3383d0f0d5f4d0e2
James O'Shea <james.oshea@workday.com>
2021-12-05 15:13:17 +0000

    Day04 solutions

--- lib/aoc_2021/day_04.rb
+++ lib/aoc_2021/day_04.rb
@@ -4,26 +4,66 @@ module Aoc2021
   #----
   class Day04Part1
     def initialize(input_file)
-      @input = File.read(input_file).each_line(chomp: true).to_a
+      @input = File.read(input_file).each_line(chomp: true).to_a.freeze
+      @drawn = @input[0].split(',').map(&:to_i)
     end
 
     def run
-      # ---
+      @drawn.each do |n|
+        grids.each do |g|
+          g.each { |l| return winner(g, n) if check_line_for_number(l, n).sum.zero? }
+        end
+      end
+    end
+
+    def check_line_for_number(line, number)
+      line.map! { |v| v.eql?(number) ? 0 : v }
+    end
+
+    def winner(grid, drawn_number)
+      grid.flatten.sum * drawn_number
+    end
+
+    def grids
+      @grids ||= begin
+        all_grids = []
+        @input[2..].each_slice(6) do |s|
+          this_grid = s[0..4].map { |l| l.split.map(&:to_i) }
+          all_grids << this_grid
+          all_grids << this_grid.transpose
+        end
+        all_grids
+      end
     end
   end
 
   #----
   class Day04Part2 < Day04Part1
     def run
-      # ---
+      winners = []
+      @drawn.each do |n|
+        grids.each do |g|
+          next if won_already?(g)
+
+          g.each { |l| winners << winner(g, n) if check_line_for_number(l, n).sum.zero? }
+        end
+      end
+      winners.last
+    end
+
+    def won_already?(grid)
+      (@won_already ||= {})[grid] ||= begin
+        (grid + grid.transpose).each { |l| return true if l.sum.zero? }
+        false
+      end
     end
   end
 
   #-----
   module Day04
     def self.default_input_file
-      File.join(Aoc2021::INPUT_FILES, 'day_04_example.txt')
-      # File.join(Aoc2021::INPUT_FILES, 'day_04.txt')
+      # File.join(Aoc2021::INPUT_FILES, 'day_04_example.txt')
+      File.join(Aoc2021::INPUT_FILES, 'day_04.txt')
     end
 
     def self.part_one(input_file = default_input_file)
--- spec/aoc_2021_day_04_spec.rb
+++ spec/aoc_2021_day_04_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2021 do
-#   describe 'day_04' do
-#     context 'part_one_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day04.part_one(File.join(Aoc2021::INPUT_FILES, 'day_04_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_one' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day04.part_one).to be(000)
-#       end
-#     end
-#
-#     context 'part_two_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day04.part_two(File.join(Aoc2021::INPUT_FILES, 'day_04_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_two' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day04.part_two).to be(000)
-#       end
-#     end
-#   end
-# end
+RSpec.describe Aoc2021 do
+  describe 'day_04' do
+    context 'part_one_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day04.part_one(File.join(Aoc2021::INPUT_FILES, 'day_04_example.txt'))).to be(4512)
+      end
+    end
+
+    context 'part_one' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day04.part_one).to be(21_607)
+      end
+    end
+
+    context 'part_two_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day04.part_two(File.join(Aoc2021::INPUT_FILES, 'day_04_example.txt'))).to be(1924)
+      end
+    end
+
+    context 'part_two' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day04.part_two).to be(19_012)
+      end
+    end
+  end
+end
