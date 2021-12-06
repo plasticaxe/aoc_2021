35a50924873b6f68f580ac4c6290a62d379ba908
James O'Shea <james.oshea@workday.com>
2021-12-06 14:49:05 +0000

    Day06 solutions

--- lib/aoc_2021/day_06.rb
+++ lib/aoc_2021/day_06.rb
@@ -4,34 +4,50 @@ module Aoc2021
   #----
   class Day06Part1
     def initialize(input_file)
-      @input = File.read(input_file).each_line(chomp: true).to_a
+      @input = File.read(input_file).each_line(chomp: true).to_a.freeze
     end
 
-    def run
-      # ---
+    def run(times)
+      times.times { update_fish }
+      all_fish.values.sum
+    end
+
+    def update_fish
+      current_fish = all_fish.dup
+      new_fish     = current_fish[0].to_i
+      8.downto(1).each { |n| all_fish[n - 1] = current_fish[n].to_i }
+      all_fish[6] += new_fish
+      all_fish[8] = new_fish
+    end
+
+    def all_fish
+      @all_fish ||= begin
+        fish = {}
+        @input[0].split(',').map(&:to_i).each do |f|
+          fish[f] ||= 0
+          fish[f] += 1
+        end
+        fish
+      end
     end
   end
 
   #----
-  class Day06Part2 < Day06Part1
-    def run
-      # ---
-    end
-  end
+  class Day06Part2 < Day06Part1; end
 
   #-----
   module Day06
     def self.default_input_file
-      File.join(Aoc2021::INPUT_FILES, 'day_06_example.txt')
-      # File.join(Aoc2021::INPUT_FILES, 'day_06.txt')
+      # File.join(Aoc2021::INPUT_FILES, 'day_06_example.txt')
+      File.join(Aoc2021::INPUT_FILES, 'day_06.txt')
     end
 
     def self.part_one(input_file = default_input_file)
-      Aoc2021::Day06Part1.new(input_file).run
+      Aoc2021::Day06Part1.new(input_file).run(80)
     end
 
     def self.part_two(input_file = default_input_file)
-      Aoc2021::Day06Part2.new(input_file).run
+      Aoc2021::Day06Part2.new(input_file).run(256)
     end
   end
 end
--- spec/aoc_2021_day_06_spec.rb
+++ spec/aoc_2021_day_06_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2021 do
-#   describe 'day_06' do
-#     context 'part_one_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day06.part_one(File.join(Aoc2021::INPUT_FILES, 'day_06_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_one' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day06.part_one).to be(000)
-#       end
-#     end
-#
-#     context 'part_two_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day06.part_two(File.join(Aoc2021::INPUT_FILES, 'day_06_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_two' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day06.part_two).to be(000)
-#       end
-#     end
-#   end
-# end
+RSpec.describe Aoc2021 do
+  describe 'day_06' do
+    context 'part_one_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day06.part_one(File.join(Aoc2021::INPUT_FILES, 'day_06_example.txt'))).to be(5934)
+      end
+    end
+
+    context 'part_one' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day06.part_one).to be(356_190)
+      end
+    end
+
+    context 'part_two_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day06.part_two(File.join(Aoc2021::INPUT_FILES, 'day_06_example.txt'))).to be(26_984_457_539)
+      end
+    end
+
+    context 'part_two' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day06.part_two).to be(1_617_359_101_538)
+      end
+    end
+  end
+end
