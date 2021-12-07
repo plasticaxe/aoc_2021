bb3db2010b6de7c7b23df030df772c6d3101a82b
James O'Shea <james.oshea@workday.com>
2021-12-07 12:39:14 +0000

    Day07 solutions

--- lib/aoc_2021/day_07.rb
+++ lib/aoc_2021/day_07.rb
@@ -4,26 +4,31 @@ module Aoc2021
   #----
   class Day07Part1
     def initialize(input_file)
-      @input = File.read(input_file).each_line(chomp: true).to_a
+      @input = File.read(input_file).each_line(chomp: true).to_a.freeze
+      @crabs = @input[0].split(',').map(&:to_i).freeze
     end
 
     def run
-      # ---
+      @crabs.min.upto(@crabs.max).map { |p| @crabs.map { |c| (c - p).abs }.sum }.min
     end
   end
 
   #----
   class Day07Part2 < Day07Part1
     def run
-      # ---
+      @crabs.min.upto(@crabs.max).map { |p| @crabs.map { |c| fuel((c - p).abs) }.sum }.min
+    end
+
+    def fuel(change)
+      (@fuel ||= {})[change.to_s] ||= 1.upto(change).to_a.sum
     end
   end
 
   #-----
   module Day07
     def self.default_input_file
-      File.join(Aoc2021::INPUT_FILES, 'day_07_example.txt')
-      # File.join(Aoc2021::INPUT_FILES, 'day_07.txt')
+      # File.join(Aoc2021::INPUT_FILES, 'day_07_example.txt')
+      File.join(Aoc2021::INPUT_FILES, 'day_07.txt')
     end
 
     def self.part_one(input_file = default_input_file)
--- spec/aoc_2021_day_07_spec.rb
+++ spec/aoc_2021_day_07_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2021 do
-#   describe 'day_07' do
-#     context 'part_one_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day07.part_one(File.join(Aoc2021::INPUT_FILES, 'day_07_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_one' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day07.part_one).to be(000)
-#       end
-#     end
-#
-#     context 'part_two_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day07.part_two(File.join(Aoc2021::INPUT_FILES, 'day_07_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_two' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day07.part_two).to be(000)
-#       end
-#     end
-#   end
-# end
+RSpec.describe Aoc2021 do
+  describe 'day_07' do
+    context 'part_one_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day07.part_one(File.join(Aoc2021::INPUT_FILES, 'day_07_example.txt'))).to be(37)
+      end
+    end
+
+    context 'part_one' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day07.part_one).to be(349_769)
+      end
+    end
+
+    context 'part_two_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day07.part_two(File.join(Aoc2021::INPUT_FILES, 'day_07_example.txt'))).to be(168)
+      end
+    end
+
+    context 'part_two' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day07.part_two).to be(99_540_554)
+      end
+    end
+  end
+end
