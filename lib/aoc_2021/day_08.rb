b5d0c8d13aed371e305fa14409b05124ca65579f
James O'Shea <james.oshea@workday.com>
2021-12-08 13:51:02 +0000

    Day08 solutions

--- lib/aoc_2021/day_08.rb
+++ lib/aoc_2021/day_08.rb
@@ -4,26 +4,100 @@ module Aoc2021
   #----
   class Day08Part1
     def initialize(input_file)
-      @input = File.read(input_file).each_line(chomp: true).to_a
+      @input = File.read(input_file).each_line(chomp: true).to_a.freeze
     end
 
     def run
-      # ---
+      @input.map { |i| i.split('|')[1].strip.split.count { |o| [2, 4, 3, 7].include?(o.size) } }.sum
     end
   end
 
   #----
   class Day08Part2 < Day08Part1
+    #----
+    class SignalPatterns
+      def initialize(input_line)
+        @input_line  = input_line
+        @pattern_map = {}
+        @pattern_key = {}
+      end
+
+      def value_of_outputs
+        easy_patterns
+        outputs.map { |o| value_of(o) }.join.to_i
+      end
+
+      private
+
+      def outputs
+        @outputs ||= @input_line.split('|')[1].strip.split.map { |p| p.chars.sort.join }.freeze
+      end
+
+      def value_of(output)
+        @pattern_map[output] ||= figure_out(output)
+      end
+
+      def easy_patterns
+        pattern_of('1', 2)
+        pattern_of('4', 4)
+        pattern_of('7', 3)
+        pattern_of('8', 7)
+      end
+
+      def pattern_of(number, size)
+        pattern               = patterns_by_size(size)[0]
+        @pattern_map[pattern] = number
+        @pattern_key[number]  = pattern.chars
+      end
+
+      def patterns_by_size(size)
+        patterns.select { |p| p.size.eql?(size) }
+      end
+
+      def patterns
+        @patterns ||= @input_line.split('|')[0].strip.split.map { |p| p.chars.sort.join }.freeze
+      end
+
+      def figure_out(output)
+        case output.size
+        when 5
+          output_size_five(output)
+        when 6
+          output_size_six(output)
+        end
+      end
+
+      def output_size_six(output)
+        if (output.chars & @pattern_key['1']).size.eql?(1)
+          '6'
+        elsif (output.chars & @pattern_key['4']).size.eql?(4)
+          '9'
+        else
+          '0'
+        end
+      end
+
+      def output_size_five(output)
+        if (output.chars & @pattern_key['7']).size.eql?(3)
+          '3'
+        elsif (output.chars & @pattern_key['4']).size.eql?(3)
+          '5'
+        else
+          '2'
+        end
+      end
+    end
+
     def run
-      # ---
+      @input.map { |i| SignalPatterns.new(i).value_of_outputs }.sum
     end
   end
 
   #-----
   module Day08
     def self.default_input_file
-      File.join(Aoc2021::INPUT_FILES, 'day_08_example.txt')
-      # File.join(Aoc2021::INPUT_FILES, 'day_08.txt')
+      # File.join(Aoc2021::INPUT_FILES, 'day_08_example.txt')
+      File.join(Aoc2021::INPUT_FILES, 'day_08.txt')
     end
 
     def self.part_one(input_file = default_input_file)
--- spec/aoc_2021_day_08_spec.rb
+++ spec/aoc_2021_day_08_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2021 do
-#   describe 'day_08' do
-#     context 'part_one_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day08.part_one(File.join(Aoc2021::INPUT_FILES, 'day_08_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_one' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day08.part_one).to be(000)
-#       end
-#     end
-#
-#     context 'part_two_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day08.part_two(File.join(Aoc2021::INPUT_FILES, 'day_08_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_two' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day08.part_two).to be(000)
-#       end
-#     end
-#   end
-# end
+RSpec.describe Aoc2021 do
+  describe 'day_08' do
+    context 'part_one_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day08.part_one(File.join(Aoc2021::INPUT_FILES, 'day_08_example.txt'))).to be(26)
+      end
+    end
+
+    context 'part_one' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day08.part_one).to be(504)
+      end
+    end
+
+    context 'part_two_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day08.part_two(File.join(Aoc2021::INPUT_FILES, 'day_08_example.txt'))).to be(61_229)
+      end
+    end
+
+    context 'part_two' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day08.part_two).to be(1_073_431)
+      end
+    end
+  end
+end
