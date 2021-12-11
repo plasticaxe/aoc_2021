5b7c8a6f3cb498c6d62e41cf98787a0b756a5f60
James O'Shea <james.oshea@workday.com>
2021-12-11 14:21:55 +0000

    Day10 solutions

--- lib/aoc_2021/day_10.rb
+++ lib/aoc_2021/day_10.rb
@@ -4,26 +4,80 @@ module Aoc2021
   #----
   class Day10Part1
     def initialize(input_file)
-      @input = File.read(input_file).each_line(chomp: true).to_a
+      @input    = File.read(input_file).each_line(chomp: true).to_a.freeze
+      @opposite = { '(' => ')',
+                    '[' => ']',
+                    '{' => '}',
+                    '<' => '>' }
+      @values   = { ')' => 3,
+                    ']' => 57,
+                    '}' => 1197,
+                    '>' => 25_137 }
     end
 
     def run
-      # ---
+      illegal_chars.map { |c| @values[c] }.sum
+    end
+
+    def illegal_chars
+      illegals = []
+      @input.each do |s|
+        illegals << first_illegal_char(s)
+      end
+      illegals.compact
+    end
+
+    def first_illegal_char(string)
+      record = []
+      string.chars.each do |c|
+        if @opposite.keys.include?(c)
+          record.push(c)
+        else
+          return c unless c.eql?(@opposite[record.pop])
+          return nil if record.empty?
+        end
+      end
+      nil
     end
   end
 
   #----
   class Day10Part2 < Day10Part1
+    def initialize(input_file)
+      super
+      @points = { ')' => 1, ']' => 2, '}' => 3, '>' => 4 }
+    end
+
     def run
-      # ---
+      results = score_for_all_lines.compact.sort
+      results[results.size / 2]
+    end
+
+    def score_for_all_lines
+      @input.map do |string|
+        next unless first_illegal_char(string).nil?
+
+        record = []
+        string.chars.each { |c| @opposite.keys.include?(c) ? record.push(c) : record.pop }
+        score_for_rest_of_line(record)
+      end
+    end
+
+    def score_for_rest_of_line(record)
+      score = 0
+      until record.empty?
+        new_char = @opposite[record.pop]
+        score    = (score * 5) + @points[new_char]
+      end
+      score
     end
   end
 
   #-----
   module Day10
     def self.default_input_file
-      File.join(Aoc2021::INPUT_FILES, 'day_10_example.txt')
-      # File.join(Aoc2021::INPUT_FILES, 'day_10.txt')
+      # File.join(Aoc2021::INPUT_FILES, 'day_10_example.txt')
+      File.join(Aoc2021::INPUT_FILES, 'day_10.txt')
     end
 
     def self.part_one(input_file = default_input_file)
--- spec/aoc_2021_day_10_spec.rb
+++ spec/aoc_2021_day_10_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2021 do
-#   describe 'day_10' do
-#     context 'part_one_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day10.part_one(File.join(Aoc2021::INPUT_FILES, 'day_10_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_one' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day10.part_one).to be(000)
-#       end
-#     end
-#
-#     context 'part_two_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day10.part_two(File.join(Aoc2021::INPUT_FILES, 'day_10_example.txt'))).to be(000)
-#       end
-#     end
-#
-#     context 'part_two' do
-#       it 'should have the right answer' do
-#         expect(Aoc2021::Day10.part_two).to be(000)
-#       end
-#     end
-#   end
-# end
+RSpec.describe Aoc2021 do
+  describe 'day_10' do
+    context 'part_one_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day10.part_one(File.join(Aoc2021::INPUT_FILES, 'day_10_example.txt'))).to be(26_397)
+      end
+    end
+
+    context 'part_one' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day10.part_one).to be(168_417)
+      end
+    end
+
+    context 'part_two_example' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day10.part_two(File.join(Aoc2021::INPUT_FILES, 'day_10_example.txt'))).to be(288_957)
+      end
+    end
+
+    context 'part_two' do
+      it 'should have the right answer' do
+        expect(Aoc2021::Day10.part_two).to be(2_802_519_786)
+      end
+    end
+  end
+end
